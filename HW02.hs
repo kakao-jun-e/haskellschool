0a2c7ed047536a0841f03f686468a762d0428e45
Dongjun, LEE <redongjun@gmail.com>
2016-05-20 09:31:42 +0900

    Update HW02.hs

--- HW02.hs
+++ HW02.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW02 where
 
+import Data.List
+
 -- Mastermind -----------------------------------------
 
 -- A peg can be one of six colors
@@ -23,43 +25,55 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches secret guess = length $ filter (==True) $ zipWith (==) secret guess
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors guess = map (\color -> sum [1 | x<-[color], y<-guess, x==y]) colors
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches secret guess = sum $ zipWith (min) (countColors secret) (countColors guess)
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess = Move guess exact_count nonexact_count
+  where exact_count = (exactMatches secret guess)
+        nonexact_count = (matches secret guess) - exact_count
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent move secret = (==) move $ getMove secret guess
+  where Move guess _ _ = move
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move = filter (isConsistent move)
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes len
+  | len == 0 = []
+  | len == 1 = group colors
+  | otherwise = concatMap (\color -> map (color:) $ allCodes (len-1)) colors
 
 -- Exercise 7 -----------------------------------------
 
 solve :: Code -> [Move]
-solve = undefined
+solve secret = getMoves $ allCodes $ length secret
+  where
+    getMoves guesses
+      | length guesses == 1 = [move]
+      | otherwise = move : (getMoves $ filterCodes move guesses)
+      where
+        move = (getMove secret $ head guesses)
 
 -- Bonus ----------------------------------------------
 
